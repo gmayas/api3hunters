f1aa6d3e995b2ab7411ddfe3a5e49e6e3d2280da
Gabiel Maya Sanchez <isc_gmayas@hotmail.com>
2021-10-27 07:15:20 -0500

    depuracion commit

--- src/services/users.js
+++ src/services/users.js
@@ -15,12 +15,7 @@ const createUser = (req, res) => {
     name: req.body.name.trim(),
     age: req.body.age.trim(),
     city: req.body.city.trim()
-  }
-  console.log('data: ', data)
-  console.log('data.name:', Object.keys(data.name).length === 0)
-  console.log('data.age:', Object.keys(data.age).length === 0)
-  console.log('data.city:', Object.keys(data.city).length === 0)
-
+  };
   if (Object.keys(data.name).length === 0 || Object.keys(data.age).length === 0 || Object.keys(data.city).length === 0) {
      console.log('Todos los coampos son requeridos');
   } else {
